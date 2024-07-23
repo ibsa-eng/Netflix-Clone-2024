05dfe6ef757b6259df52bdc3a7a6a5cca41b8e35
ibsa-eng <104551852+ibsa-eng@users.noreply.github.com>
2024-07-23 16:51:56 -0500

    Header and Footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
+      <Footer />
     </div>
   );
 }
