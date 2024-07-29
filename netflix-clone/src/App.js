3a918c7a0d700d2598b63a1502c79c568fd289f2
ibsa-eng <104551852+ibsa-eng@users.noreply.github.com>
2024-07-28 19:39:29 -0500

    RowList

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
-import Footer from "./Components/Footer/Footer";
-import Header from "./Components/Header/Header";
+
+import Home from "./pages/Home/Home";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      <Home />
     </div>
   );
 }
